31602630090f4a595060f4875c6b46115f818a74
AKASH V <113449771+Akaashvee@users.noreply.github.com>
2023-04-13 23:58:16 +0530

    Add files via upload

---
+++ tools for datascience.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # MY JUPYTER NOTEBOOK FOR TOOLS FOR DATASCIENCE
+
+# ### Welcome to my notebook. In this notebook, I will be exploring and understading the tools which are used in the data science and will analyze and visualize the data. I wanted to learn datascience because to develop skills in machine learning and artificial intelligence and for my Interest in working with data and deriving insights from it
+
+# #### Data Science Languages include :
+# Python: Python is the most popular language in the data science community
+# 
+# R: R is a language designed specifically for statistical computing and data analysis.
+# 
+# SQL: SQL (Structured Query Language) is a language used to manage and manipulate relational databases.
+# 
+# MATLAB: MATLAB is a proprietary programming language that is commonly used in scientific computing, engineering, and data analysis.
+# 
+# Java: Java is a popular language in big data processing and analysis
+
+# #### data science libraries include :
+#     
+# NumPy: For working with arrays and numerical operations
+#     
+# Pandas: For data manipulation and analysis
+#     
+# Matplotlib: For data visualization
+#     
+# Scikit-learn: For machine learning algorithms
+#     
+# TensorFlow: For building and training deep learning models
+#     
+# PyTorch: For building and training deep learning models
+#     
+# Keras: For building and training deep learning models
+#     
+# NLTK: For natural language processing tasks
+#     
+# OpenCV: For computer vision tasks
+
+# #### Table of Data Science tools include :
+#  
+#  | Tool         | Purpose                          | Language   |
+# |--------------|----------------------------------|------------|
+# | Python       | General-purpose language         | Python     |
+# | R            | Statistical computing and graphics| R          |
+# | SQL          | Relational database management   | SQL        |
+# | Tableau      | Data visualization and business intelligence | N/A |
+# | Power BI     | Business analytics and data visualization | N/A |
+# | Excel        | Spreadsheet software             | N/A        |
+# | Apache Hadoop| Distributed computing framework | Java       |
+# | Apache Spark | Large-scale data processing      | Scala/Java |
+# | TensorFlow   | Machine learning                 | Python     |
+# | PyTorch      | Machine learning                 | Python     |
+# | Keras        | Deep learning                    | Python     |
+# | scikit-learn | Machine learning                 | Python     |
+# | NLTK         | Natural language processing      | Python     |
+# | SAS          | Analytics software               | SAS        |
+# | MATLAB       | Numerical computing              | MATLAB     |
+# | RapidMiner   | Data mining and predictive analytics | Java    |
+# | Weka         | Data mining and machine learning | Java       |
+# 
+
+# #### Arithmetic expression :
+#     
+#  Addition: `1 + 2 = 3`
+#  Subtraction: `9 - 1 = 8`
+#  Multiplication: `6 * 2 = 12`
+#  Division: `45 / 5 = 9`
+
+# In[1]:
+
+
+#To multiply and add numbers
+a = 4
+b = 5
+
+# multiply a and b
+c = a * b
+print("Multiplication of", a, "and", b, "is", c)
+
+# add a and b
+d = a + b
+print("Addition of", a, "and", b, "is", d)
+
+
+# In[2]:
+
+
+#To convert minutes to hours. 
+minutes = 120  # assign a value of 120 to the variable minutes
+
+hours = minutes / 60  # convert minutes to hours
+print(minutes, "minutes is equal to", hours, "hours")
+
+
+# #### Objectives
+# Understand the basic concepts and principles of data science
+# 
+# Learn how to perform data analysis and visualization using Python and R
+# 
+# Understand the different techniques and algorithms used in machine learning
+# 
+# Build predictive models using machine learning algorithms
+# 
+# Learn how to evaluate and fine-tune machine learning models
+# 
+# Understand the ethical and legal considerations in data science
+# 
+# Apply data science techniques to real-world problems and scenarios
+
+# ## Author: Akshaya
+
+# In[ ]:
+
+
+
+
